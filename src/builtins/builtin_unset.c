bd409994db245fc089b64340047c52ac12acf60b
Andrey Zaytsev <blakc.mirror@gmail.com>
2026-05-21 09:19:12 +0200

    fix(unset): match bash no-op semantics

--- src/builtins/builtin_unset.c
+++ src/builtins/builtin_unset.c
@@ -12,46 +12,15 @@
 
 #include "minishell.h"
 
-static int	is_valid_identifier(const char *s)
-{
-	int	i;
-
-	if (!s || !s[0])
-		return (0);
-	if (!ft_isalpha(s[0]) && s[0] != '_')
-		return (0);
-	i = 1;
-	while (s[i])
-	{
-		if (!ft_isalnum(s[i]) && s[i] != '_')
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-static int	unset_invalid(const char *arg)
-{
-	ft_putstr_fd("minishell: unset: `", 2);
-	ft_putstr_fd((char *)arg, 2);
-	ft_putendl_fd("': not a valid identifier", 2);
-	return (1);
-}
-
 int	builtin_unset(t_shell *shell, char **args)
 {
 	int	i;
-	int	status;
 
-	status = 0;
 	i = 1;
 	while (args[i])
 	{
-		if (!is_valid_identifier(args[i]))
-			status = unset_invalid(args[i]);
-		else
-			env_unset(&shell->env, args[i]);
+		env_unset(&shell->env, args[i]);
 		i++;
 	}
-	return (status);
+	return (0);
 }
